8ac9d00908860fd11e130b4800ea2dbfb0fbfbb3
舒洪明 <shuhm_wy@163.com>
2017-04-24 21:29:40 +0800

    修改toolkit/init_basic_info.py

--- toolkit/init_basic_info.py
+++ toolkit/init_basic_info.py
@@ -1,8 +1,13 @@
 #-*- coding: utf-8 -*-
-from util import tx
+
+import sys
+
+sys.path.append('.')
+
+from acquisition import tx
 from acquisition import basic
 
 if __name__ == '__main__':
     stock_list = tx.get_stock_list()
-    print(stock_list)
+    #print(stock_list)
     basic.save_stock_list_into_db(stock_list)
