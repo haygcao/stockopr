66b744b19dffe10c15f25969104867cd409bfce0
shuhm <shuhm.gg@gmail.com>
2021-06-16 20:50:10 +0800

    [scheduler] handle exception

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 import schedule
 import time
 
@@ -6,22 +8,38 @@ from trade_manager import trade_manager
 from util.log import logger
 
 
+def handle_exception(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logger.error('in func[{0}], error message: {1} '.format(func.__name__, e))
+    return inner
+
+
+@handle_exception
 def job():
     print("I'm working...")
+    raise Exception('xxx')
 
 
+@handle_exception
 def sync():
     trade_manager.sync()
 
 
+@handle_exception
 def save_quote():
     acquire.save_quote()
 
 
+@handle_exception
 def patrol():
     trade_manager.patrol()
 
 
+@handle_exception
 def create_position_price_limited():
     trade_manager.create_position_price_limited()
 
@@ -35,8 +53,10 @@ def schedule_task():
     # schedule.every(5).to(10).minutes.do(job)
     # schedule.every().monday.do(job)
     # schedule.every().wednesday.at("13:15").do(job)
-    schedule.every().minute.at(":00").do(patrol)
-    schedule.every().minute.at(":00").do(create_position_price_limited)
+    # schedule.every().minute.at(":00").do(patrol)
+    # schedule.every().minute.at(":00").do(create_position_price_limited)
+    schedule.every().hour.at("00:00").do(patrol)
+    schedule.every().hour.at("00:00").do(create_position_price_limited)
 
     while True:
         schedule.run_pending()
