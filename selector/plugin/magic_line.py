7f5c1a11e0f070a65af8e66ab97ff070839f59ba
shuhm <shuhm.gg@gmail.com>
2021-07-29 20:56:47 +0800

    [selector] magic line

--- selector/plugin/magic_line.py
+++ selector/plugin/magic_line.py
@@ -3,38 +3,60 @@
 """ 神奇支撑线 10周均线
 股价触碰神奇支撑线时买入
 """
+import numpy
+
 from acquisition import quote_db
+from util import util
 from util.macd import ema
 
 # 成交量缩小 50%
-vol_times = 0.5
+g_vol_times = 2
+g_up_percent = 40
+g_current_close_vs_prev_high_percent = 1.1
 
 
-def magic_line_one_day(vol, vol_ema_s, vol_ema_m, vol_ema_l, quote, ema_xs, ema_s, ema_m, ema_l, back_day):
-    emas = [ema_s, ema_m, ema_l]
+def magic_line_one_day(vol, vol_ema_s, vol_ema_m, vol_ema_l, quote, ema_current, ema_xs, ema_s, ema_m, ema_l, back_day):
     current = -1 - back_day
-    for e in emas:
-        if quote.low.iloc[current] < e.iloc[current]:
-            break
-    else:
+
+    if quote.low.iloc[current] > ema_current.iloc[current]:
         return False
 
-    #
-    if ema_xs.iloc[current + 1] < ema_xs.iloc[current]:
+    ema_series = ema_current.iloc[current - 10: current]
+    ema_series_shift = ema_series.shift(periods=1).fillna(ema_series.iloc[0])
+    if not (ema_series >= ema_series_shift).all():
+        return False
+
+    high_series = quote.high.iloc[current - 10: current]
+    prev_high = high_series.max()
+    if prev_high > quote.close.iloc[current:].max() * g_current_close_vs_prev_high_percent:
+        return False
+
+    prev_high_index = numpy.where(high_series == prev_high)[0][0]
+    # index = high_series.index[prev_high_index]
+    days = len(high_series) - prev_high_index
+
+    # 3天的涨幅
+    close_series = quote.close.iloc[current - days - 5: current - days + 1]
+    percent = (close_series.max()/close_series.min() - 1) * 100
+
+    # print('\n{} {} {} {} {}'.format(
+    #     quote.code[-1], high_series.index[prev_high_index], percent, close_series.max(), close_series.min()))
+    if percent < g_up_percent:
         return False
 
     # 10周均线向上
-    if ema_m.iloc[current] <= ema_m.iloc[-2 - back_day]:
+    if ema_m.iloc[current] <= ema_m.iloc[current - 1]:
         return False
 
     # 成交量缩小 30% ~ 50%
-    if vol.iloc[current] > vol.iloc[-10 - 1 - back_day: current].max() * vol_times:
+    vol_series = vol.iloc[current - 10: current + 1]
+    if vol_series.max() < vol_series.min() * g_vol_times:
         return False
 
     return True
 
 
-def magic_line(quote, period, back_days=3):
+def magic_line(quote, period, back_days=5):
     # 重采样为 周数据
     quote = quote_db.get_price_info_df_db_week(quote, period_type='W')
 
@@ -55,8 +77,10 @@ def magic_line(quote, period, back_days=3):
         j = i
 
     # 回退 6个月
-    for back_day in range(back_days, 1, -1):
-        if magic_line_one_day(vol_series, vol_ema_s, vol_ema_m, vol_ema_l, quote, ema_xs, ema_s, ema_m, ema_l, back_day):
-            return True
+    for back_day in range(back_days, -1, -1):
+        for e in [ema_m]: # [ema_s, ema_m, ema_l]:
+            if magic_line_one_day(vol_series, vol_ema_s, vol_ema_m, vol_ema_l, quote, e,
+                                  ema_xs, ema_s, ema_m, ema_l, back_day):
+                return True
 
     return False
