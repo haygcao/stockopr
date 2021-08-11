d6b5c06253c015382e2ea5b320e863b783b50d32
shuhm <shuhm.gg@gmail.com>
2021-08-11 08:44:35 +0800

    [init] control script

--- toolkit/initenv/run_stockopr_console.py
+++ toolkit/initenv/run_stockopr_console.py
@@ -1,15 +1,17 @@
 import os
 import subprocess
 
-
 if __name__ == "__main__":
-    root_dir = r'D:\workspace\stockopr'
-    script = 'console.py'
+    # root_dir = r'D:\workspace\stockopr'
+    # py = str(os.path.join(root_dir, 'venv', 'Scripts', 'python.exe'))
+    root_dir = os.getenv('HOME') + '/workspace/stockopr'
+    py = str(os.path.join(root_dir, 'venv', 'bin', 'python'))
 
-    py = str(os.path.join(root_dir, 'venv', 'Scripts', 'python.exe'))
+    script = 'console.py'
     script_path = os.path.join(root_dir, script)
     print(py, script_path)
     print(os.path.exists(py), os.path.exists(script_path))
 
     os.environ['PYTHONPATH'] = root_dir
-    subprocess.Popen([py, script_path], creationflags=subprocess.DETACHED_PROCESS, cwd=root_dir)
+    # subprocess.Popen([py, script_path], creationflags=subprocess.DETACHED_PROCESS, cwd=root_dir)
+    subprocess.Popen([py, script_path], cwd=root_dir)
