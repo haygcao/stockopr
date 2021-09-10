88d90a60eca0679d52b06e58a975583154486e28
shuhm <shuhm.gg@gmail.com>
2021-09-10 11:39:07 +0800

    [indicator][bugfix] ema_value, Wrong number of dimensions. values.ndim != ndim

--- indicator/ema_value.py
+++ indicator/ema_value.py
@@ -6,8 +6,14 @@ g_percent = 3
 
 
 def ema_value(quote, period, s, l):
-    ema_s = ema(quote, s)
-    ema_l = ema(quote, l)
+    # 使用以下方式计算 ema 时, 在计算 percent = 100 * (1 - ema_l / ema_s) 后,
+    # 会导致 quote['ema_value'] = quote['ema_value'].mask(mask_final, quote['low']) 报错
+    # "Wrong number of dimensions. values.ndim != ndim
+    # ema_s = ema(quote, s)
+    # ema_l = ema(quote, l)
+
+    ema_s = quote.close.ewm(span=s, adjust=False).mean()
+    ema_l = quote.close.ewm(span=l, adjust=False).mean()
 
     ema_s_shift = ema_s.shift(periods=1)
     ema_l_shift = ema_l.shift(periods=1)
@@ -16,6 +22,7 @@ def ema_value(quote, period, s, l):
     diff_shift = ema_s_shift - ema_l_shift
 
     percent = 100 * (1 - ema_l / ema_s)
+    # percent = 100 * (1 - l / s)
 
     mask1 = ema_l >= ema_l_shift
     mask2 = ema_l <= ema_s
@@ -27,9 +34,10 @@ def ema_value(quote, period, s, l):
     # Do `left, right = left.align(right, axis=1, copy=False)` before e.g. `left == right`
     # mask5 = ema_s >= quote.low
     # mask4 = quote_copy.ema26 / ema26_shift5 > config.period_ema26_oscillation_threshold_map[period]
-    mask = mask1 & mask2 & mask3 & mask4
+    mask_final = mask1 & mask2 & mask3 & mask4
+    # mask_final.to_csv('mask1.csv')
 
     quote.insert(len(quote.columns), 'ema_value', numpy.nan)
-    quote['ema_value'] = quote['ema_value'].mask(mask, quote['low'])
+    quote['ema_value'] = quote['ema_value'].mask(mask_final, quote['low'])
 
     return quote
