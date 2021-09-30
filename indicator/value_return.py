ed9575597fdf16e8c1a56b2c6d1eafe486979daa
shuhm <shuhm.gg@gmail.com>
2021-09-30 23:00:20 +0800

    [indicator] value return - [条件] 由 [mask] 处理, 指标只计算[快慢EMA]先靠近再发散的点

--- indicator/value_return.py
+++ indicator/value_return.py
@@ -16,40 +16,40 @@ def value_return(quote, period):
 
     quote.insert(len(quote.columns), 'value_return', numpy.nan)
     for s, l in [(12, 26), ]:
-        # ema_s = quote.close.ewm(span=s, adjust=False).mean()
-        # ema_l = quote.close.ewm(span=l, adjust=False).mean()
-        ema_s = quote['ma{}'.format(s)]
-        ema_l = quote['ma{}'.format(l)]
+        ema_s = quote.close.ewm(span=s, adjust=False).mean()
+        ema_l = quote.close.ewm(span=l, adjust=False).mean()
+        # ema_s = quote['ma{}'.format(s)]
+        # ema_l = quote['ma{}'.format(l)]
 
         ema_s_shift = ema_s.shift(periods=1)
         ema_l_shift = ema_l.shift(periods=1)
 
+        # diff 可看作 MACD
         diff = ema_s - ema_l
         diff_shift1 = diff.shift(periods=1)
         diff_shift2 = diff.shift(periods=2)
 
-        percent = 100 * (1 - ema_l_shift / ema_s_shift)
-        # percent = 100 * (1 - l / s)
-
         mask_nan = quote['value_return'].isna()
-        # 慢速ma 在上升
-        mask1 = ema_l >= ema_l_shift
 
-        # 快速ma 大于 慢速ma
-        mask2 = ema_l <= ema_s
+        # # 慢速ma 在上升   # EMA26 向上
+        # mask1 = ema_l >= ema_l_shift
+
+        # # 快速ma 大于 慢速ma   # MACD > 0
+        # mask2 = diff > 0
 
         # 快速ma 比 快速ma 大, 但小于 3%
+        # percent = 100 * (1 - ema_l_shift / ema_s_shift)
         # mask3 = util.almost_equal(ema_s, ema_l, 1)
-        mask3 = (percent < g_percent) & (percent > 0)
+        # mask3 = (percent < g_percent) & (percent > 0)
 
-        # 快速ma 与 慢速ma 在靠近
-        mask4 = (diff > diff_shift1) & (diff_shift1 < diff_shift2)
+        # 快速ma 与 慢速ma 在靠近   # MACD 向下/走平 -> 再向上
+        mask4 = (diff > diff_shift1) & (diff_shift1 <= diff_shift2)
         # FutureWarning: Automatic reindexing on DataFrame vs Series comparisons is deprecated
         # and will raise ValueError in a future version.
         # Do `left, right = left.align(right, axis=1, copy=False)` before e.g. `left == right`
         # mask5 = ema_s >= quote.low
         # mask4 = quote_copy.ema26 / ema26_shift5 > config.period_ema26_oscillation_threshold_map[period]
-        mask_final = mask1 & mask2 & mask3 & mask4 & mask_nan
+        mask_final = mask4 & mask_nan   # & mask3
         # mask_final.to_csv('mask1.csv')
 
         quote['value_return'] = quote['value_return'].mask(mask_final, l)
